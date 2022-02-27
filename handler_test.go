46eeee1d9cc62976f16546762e820f2f6bbba24e
insectdie <ompusungguandry@gmail.com>
2022-02-27 13:31:28 +0700

    Request

--- handler_test.go
+++ handler_test.go
@@ -48,3 +48,21 @@ func TestServeMux(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		// logic web
+		fmt.Fprint(writer, request.Method)
+		fmt.Fprint(writer, request.RequestURI)
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
